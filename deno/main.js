1d294cf9d259a0c730a7fc55b213b18b94fdce9d
Taisuke Fukuno <fukuno@jig.jp>
2025-08-06 01:11:12 +0900

    added to update JSONLD

--- deno/main.js
+++ deno/main.js
@@ -1,9 +1,11 @@
 import { makeSpotCSV } from "./makeSpotCSV.js";
+import { makeJSONLD } from "./makeJSONLD.js";
 import { makeDinoSpotCSV } from "./makeDinoSpotCSV.js";
 import { makeErrSpotCSV } from "./makeErrSpotCSV.js";
 import { makeForeginSpotCSV } from "./makeForeginSpotCSV.js";
 
 await makeSpotCSV();
+await makeJSONLD();
 await makeDinoSpotCSV();
 await makeErrSpotCSV();
 await makeForeginSpotCSV();
--- deno/makeJSONLD.js
+++ deno/makeJSONLD.js
@@ -1,10 +1,16 @@
 import { CSV } from "https://js.sabae.cc/CSV.js";
 import { JSONLD } from "./JSONLD.js";
 
-const data = CSV.toJSON(await CSV.fetch("../fuku-e-spot.csv"));
+export const makeJSONLD = async () => {
+  const data = CSV.toJSON(await CSV.fetch("../fuku-e-spot.csv"));
 
-const vocab = "fuku-e-spot-schema.jsonld";
-const jsonld = JSONLD.make(vocab, data);
-console.log(jsonld);
+  const vocab = "fuku-e-spot-schema.jsonld";
+  const jsonld = JSONLD.make(vocab, data);
+  console.log(jsonld);
 
-await Deno.writeTextFile("../fuku-e-spot.jsonld", JSON.stringify(jsonld, null, 2));
+  await Deno.writeTextFile("../fuku-e-spot.jsonld", JSON.stringify(jsonld, null, 2));
+};
+
+if (import.meta.main) {
+  await makeJSONLD();
+}
